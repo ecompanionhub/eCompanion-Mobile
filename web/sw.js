4d83c1956f120b1cb02b6cb72ebc2885eb51eb58
ecompanionhub <ecompanion@zohomail.eu>
2026-09-10 05:44:42 +0200

    feat: make Lola mobile client presence-first

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ecompanion-mobile-v5';
-const SHELL = ['./', './index.html', './app.js', './attachments.js', './voice.js', './manifest.webmanifest'];
+const CACHE = 'ecompanion-mobile-v6';
+const SHELL = ['./', './index.html', './app.js', './attachments.js', './voice.js', './manifest.webmanifest', './lola-presence.webp'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
